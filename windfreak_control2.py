7a4e351f2608c20f8ef4d38a66daa564285a6233
Nick Lewty <nicklewty@gmail.com>
2015-04-16 13:01:46 +0800

    fixed indentation issues

--- windfreak_control2.py
+++ windfreak_control2.py
@@ -18,30 +18,29 @@ Code fairly simple hence no comments in the code
 import serial
 
 class windfreakusb2(object):
-    
-    baudrate = 115200
-    
-    def __init__(self, port):
-        self.serial = self._open_port(port)
-        
-    def _open_port(self, port):
-        ser = serial.Serial(port, self.baudrate, timeout=5)
-        ser.readline()
-        ser.timeout = 1
-        return ser
-
-    def _serial_write(self, string):
-        self.serial.write(string + '\n')
-    
-    def _serial_read(self):
-        msg_string = self.serial.readline()
-        # Remove any linefeeds etc
-        msg_string = msg_string.rstrip()
-        return msg_string
-    
-    def get_freq(self):
-        self._serial_write('f?')
-        return self._serial_read()
+	baudrate = 115200
+		
+	def __init__(self, port):
+		self.serial = self._open_port(port)
+		
+	def _open_port(self, port):
+		ser = serial.Serial(port, self.baudrate, timeout=5)
+		ser.readline()
+		ser.timeout = 1
+		return ser
+		
+	def _serial_write(self, string):
+		self.serial.write(string + '\n')
+		
+	def _serial_read(self):
+		msg_string = self.serial.readline()
+		# Remove any linefeeds etc
+		msg_string = msg_string.rstrip()
+		return msg_string
+		
+	def get_freq(self):
+		self._serial_write('f?')
+		return self._serial_read()
 	
 	def rf_on(self):
 		self._serial_write('o1')
@@ -51,7 +50,7 @@ class windfreakusb2(object):
 	
 	def rf_power_low(self):
 		self._serial_write('h0')
-
+	
 	def rf_power_high(self):
 		self._serial_write('h1')
 	
@@ -61,32 +60,32 @@ class windfreakusb2(object):
 	def get_pulse_mode(self,value):
 		self._serial_write('j?')
 		return self._serial_read()
-        
-    def get_power(self):
-        self._serial_write('a?')
-        return self._serial_read()
-    
-    def set_freq(self,value):
-        self._serial_write('f' + str(value))
-        
-    def check_osci(self):
-        self._serial_write('p')
-        return self._serial_read()
-								
-    def set_clock(self,value):
-        self._serial_write('x' + str(value))
-
-    def get_clock(self):
+		
+	def get_power(self):
+		self._serial_write('a?')
+		return self._serial_read()
+		
+	def set_freq(self,value):
+		self._serial_write('f' + str(value))
+		
+	def check_osci(self):
+		self._serial_write('p')
+		return self._serial_read()
+	
+	def set_clock(self,value):
+		self._serial_write('x' + str(value))
+	
+	def get_clock(self):
 		self._serial_write('x?')
 		return self._serial_read()
-
-    def set_power(self,value):
-        self._serial_write('a' + str(value))
-    
-    def serial_number(self):
-        self._serial_write('+')
-        return self._serial_read()
-    
+	
+	def set_power(self,value):
+		self._serial_write('a' + str(value))
+	
+	def serial_number(self):
+		self._serial_write('+')
+		return self._serial_read()
+	    
 
     
     
